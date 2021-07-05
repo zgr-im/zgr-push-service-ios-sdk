c301a046287e91bef9c0f7466994f849387df903
Alex_Shubin <ashubin@zagruzka.com>
2021-07-05 16:39:21 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version:5.3
 import PackageDescription
+
 let package = Package(
     name: "ZGRImSDK",
     platforms: [
@@ -12,6 +13,7 @@ let package = Package(
     ],
     targets:[
         .binaryTarget(
-            name:"ZGRImSDK",
-            path: "ZGRImSDK.xcframework")
+            name: "ZGRImSDK",
+            url: "https://github.com/zgr-im/zgr-push-service-ios-sdk/releases/download/1.1.4/ZGRImSDK.xcframework.zip",
+            checksum: "0b9a10eecb95c27d59987c72624e997df01c3a0189ced54395c0bab7fda6324d")
     ])
